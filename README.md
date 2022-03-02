c46255996ea7f3313bb218aff006eec82b17cddf
Lucas <lucas.prochnow@meliuz.com.br>
2022-03-01 21:07:52 -0300

    feat: Implement system theme switch

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# raw-electron-playground
+# Raw ElectronJS Playground
+
+<p align="center">
+  <img src="./assets/electron-logo.png" width="150" />
+</p>
+
+## Project goal
+
+- Project developed to understand concepts and play with electronJS technology.
+
+## What it does?
+
+- It allows you to switch between dark, light and system themes;
+- Navigates between home and about pages.
+
+## Run locally
+
+```shell
+yarn 
+yarn start
+```
---
+++ src/main.js
@@ -0,0 +1,51 @@
+/**
+ * Docs: https://www.electronjs.org/docs/latest/tutorial/quick-start#opening-your-web-page-in-a-browser-window
+ */
+
+const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('src/index.html')
+
+  ipcMain.handle('dark-mode:toggle', () => {
+    if (nativeTheme.shouldUseDarkColors) {
+      nativeTheme.themeSource = 'light'
+    } else {
+      nativeTheme.themeSource = 'dark'
+    }
+    return nativeTheme.shouldUseDarkColors
+  })
+
+  ipcMain.handle('dark-mode:system', () => {
+    nativeTheme.themeSource = 'system'
+  })
+}
+
+/**
+ * Docs: https://www.electronjs.org/docs/latest/tutorial/quick-start#open-a-window-if-none-are-open-macos
+ */
+ app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+/**
+ * Docs: https://www.electronjs.org/docs/latest/tutorial/quick-start#quit-the-app-when-all-windows-are-closed-windows--linux
+ */
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
